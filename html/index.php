aae461ca78811a0d02254b4e5e75925344c5f187
DESKTOP-N4LQJKL\dusgu <dusgud711@naver.com>
2022-03-17 16:46:54 +0900

    index.phpを作成しました。

---
+++ html/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+
+<html lang="ja">
+
+
+<head>
+
+
+    <meta charset="UTF-8">
+
+
+    <title>動作確認</title>
+
+
+</head>
+
+
+<body>
+
+
+    <h1><?php
+
+
+print ("DockerでPHP環境構築しました");
+
+
+?></h1>
+
+
+</body>
+
+
+</html>
